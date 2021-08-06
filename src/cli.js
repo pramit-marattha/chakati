ae84c47202fdc97a4d69ef6c815eb55646e84f1b
pramit-marattha <pramitmarattha5497@gmail.com>
2021-08-06 10:37:51 +0545

    prompting user for missing items

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,5 @@
 import arg from "arg";
+import inquirer from "inquirer";
 
 function parseArgumnetsOptions(rawArguments) {
   let args = arg(
@@ -19,12 +20,49 @@ function parseArgumnetsOptions(rawArguments) {
   return {
     skipPrompts: args["--yes"] || false,
     git: args["--git"] || false,
-    template: args._[0] || "default",
+    template: args._[0],
     runInstall: args["--install"] || false,
   };
 }
 
-export function cli(args) {
+async function promptErrorMissingOpts(opts) {
+  const defaultTemplate = "JavaScript";
+  if (opts.skipPrompts) {
+    return {
+      ...opts,
+      template: options.template || defaultTemplate,
+    };
+  }
+  const displayOptions = [];
+  if (!opts.template) {
+    displayOptions.push({
+      type: "list",
+      name: "template",
+      message: "What template would you like to use?",
+      choices: ["JavaScript"],
+      default: defaultTemplate,
+    });
+  }
+
+  if (!opts.git) {
+    displayOptions.push({
+      type: "confirm",
+      name: "git",
+      message: "Would you like to use git?",
+      default: false,
+    });
+  }
+
+  const userInput = await inquirer.prompt(displayOptions);
+  return {
+    ...opts,
+    template: opts.template || userInput.template,
+    git: opts.git || userInput.git,
+  };
+}
+
+export async function cli(args) {
   let opts = parseArgumnetsOptions(args);
+  opts = await promptErrorMissingOpts(opts);
   console.log(opts);
 }
