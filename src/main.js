66677c6f4ed83efcc4c3333e8ab0f472f0053f5f
pramit-marattha <pramitmarattha5497@gmail.com>
2021-08-06 10:56:15 +0545

    creating a template prompts and configuring template files and directory

---
+++ src/main.js
@@ -0,0 +1,45 @@
+import chalk from "chalk";
+import fs from "fs";
+import path from "path";
+import npc from "npc";
+import { promisify } from "util";
+
+// access
+const access = promisify(fs.access);
+
+// reccusive copy
+const copy = promisify(ncp);
+
+// async function to copy template files
+async function copyTemplateFiles(opts) {
+  return copy(options.templateDirectory, opts.targetDirectory, {
+    clobber: false,
+  });
+}
+
+export async function createProject(opts) {
+  opts = {
+    ...opts,
+    targetDirectory: opts.targetDirectory || process.cwd(),
+  };
+
+  const currentTargetFileUrl = import.meta.url;
+  const templateDir = path.resolve(
+    new URL(currentTargetFileUrl).pathname,
+    "../../projectTemplates/",
+    opts.template.toLowerCase()
+  );
+  opts.templateDirectory = templateDir;
+
+  try {
+    await access(templateDir, fs.constants.R_OK);
+  } catch (err) {
+    console.log(chalk.red(`Template directory ${templateDir} does not exist`));
+    process.exit(1);
+  }
+
+  console.log("Copy project files");
+  await copyTemplateFiles(opts);
+  console.log(chalk.green(`Creating project from template ${opts.template}`));
+  return true;
+}
