76c0ccaf3abc3a7203b4912a4c7b689767af7b64
pramit-marattha <pramitmarattha5497@gmail.com>
2021-08-06 10:04:39 +0545

    parseArgumentfunction added to define alias and their types

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,28 @@
+import arg from "arg";
+
+function parseArgumnetsOptions(rawArguments) {
+  let args = arg(
+    {
+      "--git": Boolean,
+      "--help": Boolean,
+      "--yes": Boolean,
+      "--install": Boolean,
+      "--g": "--git",
+      "--h": "--help",
+      "--y": "--yes",
+      "--i": "--install",
+    },
+    {
+      argv: rawArguments.slice(2),
+    }
+  );
+  return {
+    skipPrompts: args["--yes"] || false,
+    git: args["--git"] || false,
+    template: args._[0] || "default",
+  };
+}
+
 export function cli(args) {
   console.log(args);
 }
